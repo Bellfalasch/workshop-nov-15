c8b2b50fd91eb705049a453ba207b9e7d6906298
Bobby <bellfalasch@gmail.com>
2015-11-27 11:50:33 +0100

    Part added

---
+++ src/main/resources/site/parts/user-import.js
@@ -0,0 +1,25 @@
+var libs = {
+	portal: require('/lib/xp/portal'),
+	contentLib: require('/lib/xp/content'),
+	thymeleaf: require('/lib/xp/thymeleaf'),
+	util: require('/lib/enonic/util/util')
+};
+
+// SETTINGS
+var view = resolve('user-import.html');
+
+exports.get = function(req) {
+
+	var component = portal.getComponent();
+	var content = portal.getContent();
+
+	var model = {
+		'config': component.config,
+		'content': content
+	};
+
+    return {
+    	body: thymeleaf.render(view, model)
+    };
+
+};
