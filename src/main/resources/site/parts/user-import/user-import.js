47bd522a5ebae11249d95ce75b93e7b8882f8f00
Bobby <bellfalasch@gmail.com>
2015-11-27 12:52:18 +0100

    Also list all users

--- src/main/resources/site/parts/user-import/user-import.js
+++ src/main/resources/site/parts/user-import/user-import.js
@@ -7,28 +7,65 @@ var libs = {
 };
 var view = resolve('user-import.html');
 
-var log = libs.util.log;
+var logs = libs.util.log;
+
 
 exports.get = function(req) {
 
 	var component = libs.portal.getComponent();
-	var content = libs.portal.getContent();
 
+	if ( libs.auth ) {
+		log.info("Auth-lib installed");
+	}
+/*
+	log.info("libs.auth %s", libs.auth);
+	log.info("libs.auth.findPrincipals %s", libs.auth.findPrincipals);
+
+	if ( libs.auth.findPrincipals ) {
+		log.info("auth.findPrincipals found");
+	}
+*/
+
+	// Get all groups
 	var groups = libs.auth.findPrincipals({
 			type: 'group',
-			userStore: 'user-store',
+			userStore: 'system',
 			start: 0,
 			count: 100
 		});
 
 	if ( groups ) {
-		log(groups);
+		log.info("Groups found:");
+		if ( groups.total > 0 ) {
+			log.info( groups.total + " found!" );
+		} else {
+			log.info("Empty result");
+		}
+		logs(groups);
+	}
+
+	// Get all users
+	var users = libs.auth.findPrincipals({
+			type: 'user',
+			userStore: 'system',
+			start: 0,
+			count: 100
+		});
+
+	if ( users ) {
+		log.info("Users found:");
+		if ( users.total > 0 ) {
+			log.info( users.total + " found!" );
+		} else {
+			log.info("Empty result");
+		}
+		logs(users);
 	}
 
 	var model = {
 		'config': component.config,
-		'content': content,
-		'groups': groups
+		'groups': groups,
+		'users': users
 	};
 
     return {
