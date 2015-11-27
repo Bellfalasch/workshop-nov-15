4f38b53ce4365aa648c9049da30bd2006167c666
Bobby <bellfalasch@gmail.com>
2015-11-27 12:05:35 +0100

    Adding auth lib test

--- src/main/resources/site/parts/user-import/user-import.js
+++ src/main/resources/site/parts/user-import/user-import.js
@@ -2,23 +2,35 @@ var libs = {
 	portal: require('/lib/xp/portal'),
 	contentLib: require('/lib/xp/content'),
 	thymeleaf: require('/lib/xp/thymeleaf'),
+	auth: require('/lib/xp/auth'),
 	util: require('/lib/enonic/util/util')
 };
 var view = resolve('user-import.html');
 
+var log = libs.util.log;
 
 exports.get = function(req) {
 
-	var component = portal.getComponent();
-	var content = portal.getContent();
+	var component = libs.portal.getComponent();
+	var content = libs.portal.getContent();
+
+	var users = libs.auth.authLib.findPrincipals({
+			type: 'group',
+			userStore: 'user-store',
+			start: 0,
+			count: 100
+		});
+
+	log(users);
 
 	var model = {
 		'config': component.config,
-		'content': content
+		'content': content,
+		'users': users
 	};
 
     return {
-    	body: thymeleaf.render(view, model)
+    	body: libs.thymeleaf.render(view, model)
     };
 
 };
